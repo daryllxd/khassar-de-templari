ec980f278627dcd38327018b34e9755db199e29b
Daryll Santos <daryll.santos@gmail.com>
2018-05-07 18:57:24 +0800

    Add Gallery View to Books.

--- src/containers/books.js
+++ src/containers/books.js
@@ -1,5 +1,43 @@
 import React, { Component } from 'react';
+import { Grid, Row, Col, Clearfix, Panel } from 'react-bootstrap';
+import '.././stylesheets/books.css';
 import axios from 'axios';
+
+class Book extends Component {
+  render() {
+    return (
+      <Col xs={12} md={3}>
+        <Panel bsStyle="success books-gallery-panel">
+          <Panel.Heading>
+            <Panel.Title componentClass="h3">{ this.props.book.title }</Panel.Title>
+          </Panel.Heading>
+          <Panel.Body>
+            <p>{ this.props.book.subtitle }</p>
+            <p>{ this.props.book.authors.join(', ') }</p>
+            <img src={ this.props.book.thumbnail_url }/>
+          </Panel.Body>
+        </Panel>
+
+      </Col>
+    );
+  }
+}
+
+class BooksGallery extends Component {
+  render() {
+    return (
+      <Row>
+        <div className="books-gallery-container">
+          {
+            this.props.books.map( (book) =>
+                <Book key={book.id} book={book}></Book>
+            )
+          }
+        </div>
+      </Row>
+    );
+  }
+}
 
 export default class Books extends Component {
   state = {
@@ -18,13 +56,11 @@ export default class Books extends Component {
       })
   }
 
-  render () {
+  render() {
     return (
-      <div className="HomeBody">
-        <h1>Books</h1>
-        <ul>
-          { this.state.books.map( (book) => <li>{ book.title }</li>)  }
-        </ul>
+      <div className='container'>
+        <h1>Books I've Read:</h1>
+        <BooksGallery books={ this.state.books }/>
       </div>
     );
   }
