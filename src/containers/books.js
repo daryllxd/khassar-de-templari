520005dad4a4616dafc1215ad934c61e0cd74602
Daryll Santos <daryll.santos@gmail.com>
2018-05-07 17:15:22 +0800

    Added axios/retrieve books from server.

--- src/containers/books.js
+++ src/containers/books.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class Books extends Component {
+  state = {
+    books: []
+  }
+
+  componentDidMount() {
+    axios({
+      method:'get',
+      url:'http://localhost:3000/api/v1/books',
+      headers: { 'Accept' :  "application/json, text/plain, version=1 */*" }
+    })
+      .then( (res) => {
+        const books = res.data.books;
+        this.setState({ books });
+      })
+  }
+
   render () {
     return (
       <div className="HomeBody">
         <h1>Books</h1>
+        <ul>
+          { this.state.books.map( (book) => <li>{ book.title }</li>)  }
+        </ul>
       </div>
     );
   }
