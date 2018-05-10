c55635116328ddc35b2ed402f066557d3f088968
Daryll Santos <daryll.santos@gmail.com>
2018-05-10 20:38:48 +0800

    Added interceptor that Toasts 'Unauthorized if 401.'

--- src/utilities/axios.js
+++ src/utilities/axios.js
@@ -1,6 +1,16 @@
+import { toast } from 'react-toastify';
 import axios from 'axios';
 
 axios.defaults.baseURL = 'http://localhost:3000';
 axios.defaults.headers.common['Accept'] = "application/json, text/plain, version=1 */*";
 
+axios.interceptors.response.use(undefined, (error) => {
+  if(error.response.status === 401) {
+    toast.error('Unauthorized.')
+    console.log(error.response);
+    return Promise.reject(error);
+  }
+});
+
 export default axios;
+
