360bf7e5f6fe94698a46f52efcc5bab8db2f4edc
Daryll Santos <daryll.santos@gmail.com>
2018-05-11 17:07:54 +0800

    Convered `App` component to functional state component.

--- src/App.js
+++ src/App.js
@@ -9,18 +9,16 @@ import Expenses from './containers/expenses'
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <ToastContainer />
-        <NavBar />
-        <Route exact path="/pomodoros" component={PomodorosApp} />
-        <Route exact path="/expenses" component={Expenses} />
-        <Route exact path="/books" component={BooksApp} />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <ToastContainer />
+      <NavBar />
+      <Route exact path="/pomodoros" component={PomodorosApp} />
+      <Route exact path="/expenses" component={Expenses} />
+      <Route exact path="/books" component={BooksApp} />
+    </div>
+  );
 }
 
 export default App;
