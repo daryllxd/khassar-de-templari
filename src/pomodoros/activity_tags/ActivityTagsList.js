e866ad96a68a2a3a555bb75d0ee06fbb7fcf0d16
Daryll Santos <daryll.santos@gmail.com>
2018-05-16 20:47:32 +0800

    Added sorting to ActivityTags.

--- src/pomodoros/activity_tags/ActivityTagsList.js
+++ src/pomodoros/activity_tags/ActivityTagsList.js
@@ -1,25 +1,33 @@
-import React from 'react';
-import {Row, Table} from 'react-bootstrap';
+import React, { Component } from 'react';
+import { Row, Table } from 'react-bootstrap';
 import ActivityTagsListItem from 'pomodoros/activity_tags/ActivityTagsListItem'
 
-function ActivityTagsList({activityTags}) {
-  return (
-    <Table striped condensed>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Keyboard Shortcut</th>
-        </tr>
-      </thead>
-      <tbody>
-        {
-          activityTags.map ( (activityTag) =>
-              <ActivityTagsListItem key={activityTag.id} activityTag={activityTag}/>
-          )
-        }
-      </tbody>
-    </Table>
-  );
+class ActivityTagsList extends Component {
+  sort(field) {
+    this.props.sortBy(field)
+  }
+
+  render() {
+    let { activityTags } = this.props;
+
+    return (
+      <Table striped condensed>
+        <thead>
+          <tr>
+            <th onClick={this.sort.bind(this, 'name')}>Name</th>
+            <th onClick={this.sort.bind(this, 'shortcut')}>Keyboard Shortcut</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            activityTags.map ( (activityTag) =>
+                <ActivityTagsListItem key={activityTag.id} activityTag={activityTag}/>
+            )
+          }
+        </tbody>
+      </Table>
+    );
+  }
 }
 
 export default ActivityTagsList;
--- src/pomodoros/activity_tags/index.js
+++ src/pomodoros/activity_tags/index.js
@@ -6,14 +6,21 @@ import ActivityTagsList from 'pomodoros/activity_tags/ActivityTagsList';
 
 class ActivityTagsView extends Component {
   state = {
-    activity_tags: []
+    activityTags: []
+  }
+
+  sortBy = (field) => {
+    this.setState({
+      activityTags: this.state.activityTags.sort((a, b) => a[field].localeCompare(b[field])
+      )
+    })
   }
 
   componentDidMount() {
     axios({ method:'get', url:'/api/v1/activity_tags' })
       .then( (res) => {
-        const activity_tags = res.data.data.activity_tags;
-        this.setState({activity_tags})
+        const activityTags = res.data.data.activity_tags;
+        this.setState({activityTags})
       })
   }
 
@@ -21,7 +28,7 @@ class ActivityTagsView extends Component {
     return (
       <React.Fragment>
         <ActivityTagsHeader />
-        <ActivityTagsList activityTags={this.state.activity_tags} />
+        <ActivityTagsList activityTags={this.state.activityTags} sortBy={this.sortBy}/>
       </React.Fragment>
     );
   }
