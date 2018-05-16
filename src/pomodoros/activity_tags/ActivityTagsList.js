f56de5c059334627e5c71382c43035657b9e5aea
Daryll Santos <daryll.santos@gmail.com>
2018-05-16 21:24:55 +0800

    Removed unnecessary `sort` function in ActivityTagsList.

--- src/pomodoros/activity_tags/ActivityTagsList.js
+++ src/pomodoros/activity_tags/ActivityTagsList.js
@@ -3,19 +3,15 @@ import { Row, Table } from 'react-bootstrap';
 import ActivityTagsListItem from 'pomodoros/activity_tags/ActivityTagsListItem'
 
 class ActivityTagsList extends Component {
-  sort(field) {
-    this.props.sortBy(field)
-  }
-
   render() {
-    let { activityTags } = this.props;
+    let { activityTags, sortBy } = this.props;
 
     return (
       <Table striped condensed>
         <thead>
           <tr>
-            <th onClick={this.sort.bind(this, 'name')}>Name</th>
-            <th onClick={this.sort.bind(this, 'shortcut')}>Keyboard Shortcut</th>
+            <th onClick={sortBy.bind(this, 'name')}>Name</th>
+            <th onClick={sortBy.bind(this, 'shortcut')}>Keyboard Shortcut</th>
           </tr>
         </thead>
         <tbody>
